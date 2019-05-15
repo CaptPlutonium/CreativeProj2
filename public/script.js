0d230fbfb54cce3eab53caca6428457b95e40ae7
Capt <ethan.ne.son.us@gmail.com>
2019-05-15 15:47:00 -0700

    fixed the links

--- public/script.js
+++ public/script.js
@@ -1,20 +1,16 @@
-window.onload = function()
+async function  myFunct()
 {
-	document.getElementById("weatherSubmit").addEventListener("click", async function(event) {
-		event.preventDefault();
-		const value = document.getElementById("weatherInput").value;
-		if (value === "")
-		return;
-		console.log(value);
-
-		const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=eb6a614197115848be0ba229c91e6df7";
+    console.log("ok");
+	
+		const url2 = "https://cat-fact.herokuapp.com/facts/random?animal_type=cat";
         
-        const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + ",US&units=imperial" + "&APPID=eb6a614197115848be0ba229c91e6df7";
+        const url = "https://aws.random.cat/meow";
 
         var response2 = await fetch(url2);
         console.log("responce2: ",response2);
         const json2 = await response2.json();
         console.log("json2: ", json2);
+        console.log("1");
 		try{
 			//tryign to do something that might fail
 			const response = await fetch(url);
@@ -25,56 +21,30 @@ window.onload = function()
 			//console.log("json: ", json);
 
             let results = "";
-			results += '<h2>Weather in ' + json.name + "</h2>";
-            results += "<section id='mainID'>";
-			results += '<h3 id="current">Current</h2>';
-			for (let i=0; i < json.weather.length; i++) {
-				results += '<img src="https://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-			}
-			results += '<h2>' + json.main.temp + " &deg;F</h2>"
-			results += "<p id='description'>"
-			for (let i=0; i < json.weather.length; i++) {
-				results += json.weather[i].description;
-				if (i !== json.weather.length - 1)
-					results += ", ";
-			}
-            results += "</p>";
-            results += "</section>";
-			document.getElementById("weatherResults").innerHTML = results;
+            results +="<img src='" + json.file +"' class= 'picture'>"
+			
+			document.getElementById("catResults").innerHTML = results;
 
 
             //NEW STUFF
-            fetch(url2)
+           fetch(url2)
                 .then(function(response) {
                 return response.json();
-                }).then(function(json) {
+                   }).then(function(json) {
                 console.log(json);
-                let forecast = "";
-                for (let i=0; i < json.cnt; ) {
-                    var dayCount = moment(json.list[i].dt_txt).format('Do');
-                    console.log(dayCount);
-                    forecast += "<h3>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY') + "</h3>";
-                    forecast += "<table 'id=tables'><tr>";
-                    
-                    while(i < 40 && dayCount == moment(json.list[i].dt_txt).format('Do')){
-                        forecast += "<td id='table'>";
-                        forecast += "<h2 id='centerMe'>" + moment(json.list[i].dt_txt).format('h a') + "</h2>";
-                        forecast += "<p id='centerMe'>Temperature: " + json.list[i].main.temp + "</p>";
-                        forecast += "<p id='centerMe'>Wind speed: " + json.list[i].wind.speed + "</p>";
-                        forecast += '<img src="https://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
-                        forecast += "</td>";
-                        console.log(dayCount, " ", moment(json.list[i].dt_txt).format('Do'));
-                        i++;
-                    }
-                    forecast += "</tr></table>";
-                }
-                console.log(document.getElementById("forecastResults"));
-                document.getElementById("forecastResults").innerHTML = forecast;
+                let result2 = "";
+                result2 += "<p id='catFact'>"
+                result2 += json.text
+                result2 += "</p>"
+               
+
+                console.log(document.getElementById("catFactResult"));
+                document.getElementById("catFactResult").innerHTML = result2;
                 });
 
 
 		}catch(err){
 			console.log(err);
 		}
-	});
+
 };
